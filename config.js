6cdf379195190c5b7a0eff99e24919b005bf056f
Mykola,Petriv <petrivmykola3@gmail.com>
2023-12-27 11:14:40 -0500

    deleted lock file

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: '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',
+  platform: 'github',
+  onboardingConfig: {
+    extends: ['config:recommended'],
+  },
+  repositories: ['PetrivMykola/jhipster-sample-app']
+};
